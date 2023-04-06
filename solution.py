db01e7f80a0266a54e8f7d500dac646c19164f9d
JaneBelousova <42033480+JaneBelousova@users.noreply.github.com>
2023-04-06 16:27:03 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from scipy.stats import norm
 from scipy.stats import chi2
+import statistics as st
 
 chat_id = 1124136722 # Ваш chat ID, не меняйте название переменной
 
@@ -10,6 +11,8 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    df = 1
+    m = 0
+    df = 2
+    N = 2
     alpha = 1 - p
-    return (np.sqrt((x.mean()**2 ) / chi2.ppf(1 - alpha / 2, df) / 47)), (np.sqrt((x.mean()**2 ) / chi2.ppf(alpha / 2, df) / 47))
+    return np.sqrt(N * st.variance(x, m) / chi2.ppf(1 - alpha / 2, df) / 47), np.sqrt(N * st.variance(x, m) / chi2.ppf(alpha / 2, df) / 47)
