e3c180e774c3c71fa2b98aeada957cc27b6be735
JaneBelousova <42033480+JaneBelousova@users.noreply.github.com>
2023-04-20 22:01:01 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import chi2
 
 chat_id = 1124136722 # Ваш chat ID, не меняйте название переменной
 
@@ -9,7 +9,6 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    m = 0
     N = 2
     alpha = 1 - p
-    return np.sqrt(N / 47) * (x.mean() - m) / norm.ppf(alpha / 2), np.sqrt(N / 47) * (x.mean() - m) / norm.ppf(1 - alpha / 2)
+    return np.sqrt((x**2).sum() / 47 / chi2.ppf(1 - alpha / 2, 2 * len(x))), np.sqrt((x**2).sum() / 47 / chi2.ppf(alpha / 2, 2 * len(x)))
